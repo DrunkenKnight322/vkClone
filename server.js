5a0a4e2f04a948ad7087553b9b79bd8293b4cc44
rustam1998 <rostamsahatov50@gmail.com>
2018-09-30 22:32:10 +0300

    v2

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const path = require('path');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
 
 const users = require('./routes/users')
 const feed = require('./routes/feed')
@@ -10,12 +13,14 @@ const feed = require('./routes/feed')
 
 const app = express()
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(fileUpload());
+app.use('/public', express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://127.0.0.1:27017/vkclone',{ useNewUrlParser: true })
-                .then(res => console.log('MongoDB connected'))
-                .catch(err => console.log(err))
+mongoose.connect('mongodb://127.0.0.1:27017/vkclone',{ useNewUrlParser: true }).then(res => console.log('MongoDB connected'))
+
 
 //Passport middleware
 app.use(passport.initialize())
@@ -25,6 +30,22 @@ require('./config/passport')(passport)
 
 app.use('/api/users', users)
 app.use('/api/feed', feed)
+
+app.post('/upload', (req, res, next) => {
+    let imageFile = req.files.file;
+  
+    imageFile.mv(`${__dirname}/public/${req.body.filename}.jpg`, function(err) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+  
+      res.json({file: `public/${req.body.filename}.jpg`});
+    });
+  
+  })
+  app.get('/photo', (req,res) => {
+
+  })
 
 
 const port = process.env.PORT || 3030
