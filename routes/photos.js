1fa2603441b10de024587798b207e6fb38e98971
rustam1998 <rostamsahatov50@gmail.com>
2018-10-21 23:43:17 +0300

    v5

--- routes/photos.js
+++ routes/photos.js
@@ -5,7 +5,7 @@ const passport = require('passport')
 const path = require('path');
 const cors = require('cors');
 const uuidv1 = require('uuid/v1')
-const fs = require('fs-extra')
+
 
 
 const Photo = require('../models/Photo')
@@ -35,8 +35,8 @@ router.post('/upload', (req, res, next) => {
   // айди пользователя для котоорого сохранить фото
    const user_id = req.params.user_id
 
+  //const filename = photo.name
    const photo = req.files.photo
-   const filename = photo.name
    const extension = photo.mimetype.split('/')
    const generated = uuidv1() + '.' + extension[1]
 
@@ -50,25 +50,34 @@ router.post('/upload', (req, res, next) => {
        size: photo.size,
        path: `./public/${generated}`
      })
-
-     newPhoto.save().then(photo => res.json(photo)).catch(err => console.log(err))
-    // Найти пользователя по user_id
-    User.findOne({id: user_id}).
-      then(user=> {
-        Photo.find()
-        .then(photo => {
-          
-          const lastpic = photo[photo.length - 1]
-      
-            // Добавить в поле gallery, айди из новой фото
-            // СОхранить .push
-          user.gallery.push(lastpic.id)
+     //Новый код
+     newPhoto.save()
+     .then(photo => {
+       User.findOne({id: user_id})
+        .then(user => {
+          user.gallery.push(photo)
           user.save()
         })
-      })
-  
-   }) 
-})
-  
- 
+        res.json(photo)
+   })
+ })
+}
+)
+
 module.exports = router 
+
+// Старый код сохранения в галерею
+
+// User.findOne({id: user_id}).
+// then(user=> {
+//   Photo.find()
+//   .then(photo => {
+    
+//     const lastpic = photo[photo.length - 1]
+
+//       // Добавить в поле gallery, айди из новой фото
+//       // СОхранить .push
+//     user.gallery.push(lastpic.id)
+//     user.save()
+//   })
+// })
